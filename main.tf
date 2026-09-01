d3b8f968bad4f3a729bad1f46f115daf2fd5498f
NaniPrasadNani <tdpnpl219@gmail.com>
2026-09-01 08:42:38 +0530

    Initial Terraform code for Azure Resource Groups

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "5.3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "prasad" {
+  name     = "prasad"
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "prasad1" {
+  name     = "prasad1"
+  location = "West Europe"
+}
