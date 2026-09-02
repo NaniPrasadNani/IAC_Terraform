5a085b5847160ad28926c2921af71b4d9eb8318a
NaniPrasadNani <tdpnpl219@gmail.com>
2026-09-02 07:14:39 +0530

    Configure Azure backend for Terraform state
    Add Terraform backend configuration for Azure

--- main.tf
+++ main.tf
@@ -11,6 +11,15 @@ provider "azurerm" {
   features {}
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstateaccountmangala01"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
 resource "azurerm_resource_group" "prasad" {
   name     = "prasad"
   location = "West Europe"
