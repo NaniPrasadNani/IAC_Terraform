1734e5be6b171a5adca27efe265c5e5cd4c39689
NaniPrasadNani <tdpnpl219@gmail.com>
2026-09-06 10:59:42 +0530

    Add VM module configuration for production environment

--- Environmets/prod/main.tf
+++ Environmets/prod/main.tf
@@ -1,1 +1,10 @@
-
+module "infra_vm" {
+  source              = "../../modules/infra-vm"
+  vm_name             = "prod-vm1"
+  resource_group_name = "rg-prod-infra"
+  location            = var.location
+  vm_size             = "Standard_B2s"
+  admin_username      = "azureuser"
+  ssh_public_key      = var.ssh_public_key
+  subnet_id           = var.subnet_id
+}
