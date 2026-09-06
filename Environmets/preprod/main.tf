e7efc3b305b1668904a6ed3c92660674b47d000e
NaniPrasadNani <tdpnpl219@gmail.com>
2026-09-06 10:57:49 +0530

    Add VM module configuration for preprod environment

--- Environmets/preprod/main.tf
+++ Environmets/preprod/main.tf
@@ -1,1 +1,11 @@
+module "infra_vm" {
+  source              = "../../modules/infra-vm"
+  vm_name             = "preprod-vm1"
+  resource_group_name = "rg-preprod-infra"
+  location            = var.location
+  vm_size             = "Standard_B2s"
+  admin_username      = "azureuser"
+  ssh_public_key      = var.ssh_public_key
+  subnet_id           = var.subnet_id
+}
 
