4acdda55fe8decf866edd07106ff5603457af80e
NaniPrasadNani <tdpnpl219@gmail.com>
2026-09-06 10:57:13 +0530

    Add variables for VM configuration in Terraform

--- CICD_Modules/infra/variables.tf
+++ CICD_Modules/infra/variables.tf
@@ -1,1 +1,7 @@
-
+variable "vm_name" { type = string }
+variable "resource_group_name" { type = string }
+variable "location" { type = string }
+variable "vm_size" { type = string }
+variable "admin_username" { type = string }
+variable "ssh_public_key" { type = string }
+variable "subnet_id" { type = string }
