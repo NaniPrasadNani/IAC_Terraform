b0eeb9d7f3f01e644be4f59e845fcfef5fc11d8f
NaniPrasadNani <tdpnpl219@gmail.com>
2026-09-06 12:18:58 +0530

    Add variables for Azure resource configuration

--- Environmets/preprod/variables.tf
+++ Environmets/preprod/variables.tf
@@ -1,1 +1,15 @@
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+}
+
+variable "ssh_public_key" {
+  description = "Public SSH key for VM login"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "ID of the subnet where VM will be deployed"
+  type        = string
+}
 
