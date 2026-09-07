851188087445e26868001597fa2dd7be69ef5f1a
NaniPrasadNani <tdpnpl219@gmail.com>
2026-09-07 06:42:51 +0530

    Configure Azure backend and update VM module
    Added backend configuration for Azure storage and updated VM module parameters.

--- CICD_Modules/infra/main.tf
+++ CICD_Modules/infra/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "5.4.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstateaccountmangala01"
+    container_name       = "tfstate"
+    key                  = "preprod.tfstate"
+  }
 }
 
 provider "azurerm" {
@@ -17,40 +24,13 @@ resource "azurerm_resource_group" "preprod_rg" {
 }
 
 
-resource "azurerm_network_interface" "nic" {
-  name                = "${var.vm_name}-nic"
+module "infra_vm" {
+  source              = "../../CICD_Modules/infra"
+  vm_name             = "preprod-vm1"
+  resource_group_name = "rg-preprod-infra"
   location            = var.location
-  resource_group_name = var.resource_group_name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = var.subnet_id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = var.vm_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  size                = var.vm_size
-  admin_username      = var.admin_username
-  network_interface_ids = [azurerm_network_interface.nic.id]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-
-  admin_ssh_key {
-    username   = var.admin_username
-    public_key = var.ssh_public_key
-  }
+  vm_size             = "Standard_B2s"
+  admin_username      = "azureuser"
+  ssh_public_key      = var.ssh_public_key
+  subnet_id           = var.subnet_id
 }
