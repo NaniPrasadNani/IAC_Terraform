ec2a1d144d8825b5defa0802a151ab106e79db16
NaniPrasadNani <tdpnpl219@gmail.com>
2026-09-07 07:03:10 +0530

    Change VM size from Standard_B2s to Standard_B2ms

--- Environmets/preprod/main.tf
+++ Environmets/preprod/main.tf
@@ -28,7 +28,7 @@ module "infra_vm" {
   vm_name             = "preprod-vm1"
   resource_group_name = azurerm_resource_group.preprod_rg.name
   location            = var.location
-  vm_size             = "Standard_B2s"
+  vm_size             = "Standard_B2ms"
   admin_username      = "azureuser"
   ssh_public_key      = var.ssh_public_key
   subnet_id           = var.subnet_id
