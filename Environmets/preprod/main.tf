966dd83eb2eaad4ac5094d9cd51a6bd86103d709
NaniPrasadNani <tdpnpl219@gmail.com>
2026-09-07 07:31:56 +0530

    Create virtual network and subnet in preprod
    Added Azure virtual network and subnet resources for preprod environment.

--- Environmets/preprod/main.tf
+++ Environmets/preprod/main.tf
@@ -26,6 +26,21 @@ variable "vm_size" {
   type        = string
 }
 
+resource "azurerm_virtual_network" "preprod_vnet" {
+  name                = "preprod-vnet"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.preprod_rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "preprod_subnet" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.preprod_rg.name
+  virtual_network_name = azurerm_virtual_network.preprod_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+
 module "infra_vm" {
   source              = "../../CICD_Modules/infra"
   vm_name             = "preprod-vm1"
