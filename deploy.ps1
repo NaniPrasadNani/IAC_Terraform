5c1283c8787bac1392a6da78518524722247aa1b
NaniPrasadNani <tdpnpl219@gmail.com>
2026-09-17 13:55:58 +0530

    Update deploy.ps1 to include website package deployment
    Added functionality to download and extract a new website package.

--- deploy.ps1
+++ deploy.ps1
@@ -1,14 +1,21 @@
-$appUrl = "https://raw.githubusercontent.com/<username>/<repo>/main/myapp.zip"
-
+# Create Temp folder
 New-Item -ItemType Directory -Path "C:\Temp" -Force
 
+# URL of website package
+$zipUrl = "https://raw.githubusercontent.com/NaniPrasadNani/IAC_Terraform/main/wwwroot.zip"
+
+# Download application package
 Invoke-WebRequest `
--Uri $appUrl `
--OutFile "C:\Temp\myapp.zip"
+    -Uri $zipUrl `
+    -OutFile "C:\Temp\wwwroot.zip"
 
+# Extract application files to IIS root
 Expand-Archive `
--Path "C:\Temp\myapp.zip" `
--DestinationPath "C:\inetpub\wwwroot\MyApp" `
--Force
+    -Path "C:\Temp\wwwroot.zip" `
+    -DestinationPath "C:\inetpub\wwwroot" `
+    -Force
 
+# Restart IIS
 iisreset
+
+Write-Host "Application deployment completed successfully."
