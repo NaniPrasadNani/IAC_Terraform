fcc03faaf1ba254c4cb93ef4ea57bfa7afe22fef
NaniPrasadNani <tdpnpl219@gmail.com>
2026-09-17 13:15:29 +0530

    Update deployment script to download myapp.zip
    Download and extract application files to web root.

--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,14 @@
-Install-WindowsFeature Web-Server -IncludeManagementTools
+$appUrl = "https://raw.githubusercontent.com/<username>/<repo>/main/myapp.zip"
 
-New-Item -ItemType Directory -Path C:\inetpub\wwwroot -Force
+New-Item -ItemType Directory -Path "C:\Temp" -Force
 
-Set-Content `
- -Path C:\inetpub\wwwroot\index.html `
- -Value "VMSS Auto Deployment Success"
+Invoke-WebRequest `
+-Uri $appUrl `
+-OutFile "C:\Temp\myapp.zip"
+
+Expand-Archive `
+-Path "C:\Temp\myapp.zip" `
+-DestinationPath "C:\inetpub\wwwroot\MyApp" `
+-Force
+
+iisreset
