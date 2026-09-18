f01e7e9075742372171c9caf3a0a2c86b4ddecc2
NaniPrasadNani <tdpnpl219@gmail.com>
2026-09-18 09:05:01 +0530

    Update deploy.ps1 to improve IIS setup and deployment

--- deploy.ps1
+++ deploy.ps1
@@ -1,21 +1,26 @@
+# Install IIS
+Install-WindowsFeature -Name Web-Server -IncludeManagementTools
+
 # Create Temp folder
 New-Item -ItemType Directory -Path "C:\Temp" -Force
 
-# URL of website package
+# Download application package
 $zipUrl = "https://raw.githubusercontent.com/NaniPrasadNani/IAC_Terraform/main/wwwroot.zip"
 
-# Download application package
 Invoke-WebRequest `
     -Uri $zipUrl `
     -OutFile "C:\Temp\wwwroot.zip"
 
-# Extract application files to IIS root
+# Clear default IIS files
+Remove-Item "C:\inetpub\wwwroot\*" -Recurse -Force -ErrorAction SilentlyContinue
+
+# Extract website
 Expand-Archive `
     -Path "C:\Temp\wwwroot.zip" `
     -DestinationPath "C:\inetpub\wwwroot" `
     -Force
 
-# Restart IIS
-iisreset
+# Start IIS
+Start-Service W3SVC
 
 Write-Host "Application deployment completed successfully."
